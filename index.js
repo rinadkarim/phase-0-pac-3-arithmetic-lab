d5224b716812318e9c84653d414b407cc4958814
rinadkarim <rinadjobs@gmail.com>
2023-11-07 17:37:33 -0500

    Arithmetic Lab

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(){
+}
+function subtract(){
+    }
+    function multiply(){
+ }
+ function divide(){
+}
+function add(){
+    return "something";
+}
+function add(a, b){
+    return "something";
+}
+function add(a,b){
+    return a + b;
+}
+function subtract(a,b){
+    return a -b;
+}
+function multiply(a,b){
+    return a *b;
+}
+function divide(a,b){
+    return a /b;
+}
+function increment(number){
+return number+=1;
+}
+function decrement(number){
+    return number-+1;
+}
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
